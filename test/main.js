e032dbde63605763f8ee5c87d4e3dee09d60b0af
JonnyJong <jonnyjong@outlook.com>
2023-10-29 11:05:23 +0800

    update: test

--- test/main.js
+++ test/main.js
@@ -1,7 +1,12 @@
 const renderer = require('../scripts/index');
-const md = `Hello World
-Next line.
+const fs = require('fs/promises');
+const path = require('path');
 
-Other para`;
+async function main() {
+  const md = await fs.readFile(path.join(__dirname, 'test.md'), 'utf-8');
+  const { content } = await renderer.render(md, {});
+  const html = `<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><article>${content}</article></body></html>`;
+  fs.writeFile(path.join(__dirname, 'test.html'), html, 'utf-8');
+}
 
-renderer.render(md, {}).then(console.log);
+main();
