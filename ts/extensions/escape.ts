66c5bceee707209b217ab818f544bbc094b05e3a
Jonny <jonnyjong@outlook.com>
2024-06-06 14:30:44 +0800

    ✨ feat: escape more chars

--- ts/extensions/escape.ts
+++ ts/extensions/escape.ts
@@ -1,25 +1,32 @@
-import { IExtension } from "../../types/dev"
+import { IExtension } from '../../types/dev';
 
-const rule = {
-  markdown: /(\\\\|\\`|\\\*|\\_|\\\{|\\\}|\\\[|\\\]|\\\(|\\\)|\\#|\\\+|\\\-|\\\.|\\\!|\\\||\\\<|\\>|\\&){1}/,
-  html: /(\\\<|\\>|\\&){1}/,
-}
+const RULE = /\\./;
+
+const HTML_ESCAPE_MAP: { [k: string]: string } = {
+  '<': '&lt;',
+  '>': '&gt;',
+  '&': '&amp;',
+};
+
 const escapeMarkdown: IExtension = {
   name: 'escape',
   level: 'inline',
   priority: 0,
   start(src, variables) {
-    return src.match(rule.markdown)?.index;
+    return src.match(RULE)?.index;
   },
   match(src, variables) {
-    let raw = src.match(rule.markdown)?.[0];
+    let raw = src.match(RULE)?.[0];
     if (!raw) return;
-    return{
+    return {
       raw,
       text: raw.slice(1),
     };
   },
   render(matched, variables) {
+    if (matched.text in HTML_ESCAPE_MAP) {
+      return HTML_ESCAPE_MAP[matched.text];
+    }
     return matched.text;
   },
 };
