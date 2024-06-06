60f3b92ad76821ad8dfb591292c77e5fc72937c7
Jonny <jonnyjong@outlook.com>
2024-06-06 14:21:31 +0800

    ✨ feat: legacy line breaks
    #1

--- ts/renderer.ts
+++ ts/renderer.ts
@@ -1,6 +1,6 @@
-import { IVariables } from "../types/dev";
-import { Toc } from "./toc";
-import { walkBlocks, walkLine } from "./tokenizer";
+import { IVariables } from '../types/dev';
+import { Toc } from './toc';
+import { walkBlocks, walkLine } from './tokenizer';
 
 export function createVariables(options: any = {}): IVariables {
   return {
@@ -9,7 +9,10 @@ export function createVariables(options: any = {}): IVariables {
   };
 }
 
-export async function renderLine(source: string, iVar: IVariables = createVariables()): Promise<{content: string, variables: IVariables}> {
+export async function renderLine(
+  source: string,
+  iVar: IVariables = createVariables()
+): Promise<{ content: string; variables: IVariables }> {
   source = source.replace(/\r/g, '');
 
   let matcheds = await walkLine(source, iVar);
@@ -33,40 +36,53 @@ export async function renderLine(source: string, iVar: IVariables = createVariab
     offset = i;
   }
   content += source.slice(offset);
-  
+
   return { content, variables: iVar };
 }
 
-async function renderLines(source: string, iVar: IVariables, para: boolean = true): Promise<string> {
+async function renderLines(
+  source: string,
+  iVar: IVariables,
+  para: boolean = true
+): Promise<string> {
   let lines = source.split('\n');
 
   let content = '';
   let inPara = false;
+  let shouldWarp = false;
   for (const line of lines) {
     if (line === '') {
+      shouldWarp = false;
       if (inPara) {
         inPara = false;
-        if (para) content += '</p>';;
-      };
+        if (para) content += '</p>';
+      }
       continue;
     }
 
     if (!inPara) {
       inPara = true;
+      shouldWarp = false;
       if (para) content += '<p>';
-    } else {
+    } else if (!iVar.options.legacy_line_breaks) {
+      content += '<br>';
+    } else if (shouldWarp) {
       content += '<br>';
     }
-
     content += (await renderLine(line, iVar)).content;
+    shouldWarp = line.endsWith('  ');
   }
   if (inPara && para) content += '</p>';
   return content;
 }
 
-export async function render(source: string, iVar: IVariables = createVariables(), para: boolean = true): Promise<{content: string, variables: IVariables}> {
+export async function render(
+  source: string,
+  iVar: IVariables = createVariables(),
+  para: boolean = true
+): Promise<{ content: string; variables: IVariables }> {
   source = source.replace(/\r/g, '');
-  
+
   let matcheds = await walkBlocks(source, iVar);
 
   if (matcheds.length === 0) {
--- types/dev.d.ts
+++ types/dev.d.ts
@@ -16,16 +16,20 @@ export type IVariables = {
    * @readonly
    */
   options: {
-    code_highlight?: (src: string, language?: string)=>IAsyncReturnValue<{content: string, language: string}>,
-    footnote_class?: string,
-    heading_author_prefix?: string,
-    todo_list_class?: string,
-    [x: string | number | symbol]: any,
-  },
+    code_highlight?: (
+      src: string,
+      language?: string
+    ) => IAsyncReturnValue<{ content: string; language: string }>;
+    footnote_class?: string;
+    heading_author_prefix?: string;
+    todo_list_class?: string;
+    legacy_line_breaks?: boolean;
+    [x: string | number | symbol]: any;
+  };
   /**
    * 目录
    */
-  toc: IToc,
+  toc: IToc;
   /**
    * 其他由扩展自定义的变量
    */
@@ -39,20 +43,20 @@ export type IMatched = {
    * 原始的匹配结果
    * 渲染器会根据此匹配结果对源文本进行裁剪等操作
    */
-  raw: string,
+  raw: string;
   /**
    * 除去格式的文本
    * 由扩展自行操作
    */
-  text: string,
+  text: string;
   /**
    * 参数列表
    */
-  args?: string[],
+  args?: string[];
   /**
    * 参数
    */
-  arg?: string,
+  arg?: string;
   /**
    * 其他数据
    */
@@ -66,13 +70,13 @@ export type IExtension<> = {
    * 扩展名
    * 存在名称相同的已注册扩展时，旧扩展会覆盖新扩展
    */
-  name: string,
+  name: string;
   /**
    * 级别
    * block：块状
    * inline：行内
    */
-  level: 'block' | 'inline',
+  level: 'block' | 'inline';
   /**
    * 匹配起始位置
    * @param src 源文本
@@ -86,7 +90,10 @@ export type IExtension<> = {
    * @param variables 渲染时可能需要使用的变量
    * @returns 返回匹配的结果，返回 Empty 类型代表匹配失败
    */
-  match(src: string, variables: IVariables): IAsyncReturnValue<IMatched | IEmpty>;
+  match(
+    src: string,
+    variables: IVariables
+  ): IAsyncReturnValue<IMatched | IEmpty>;
   /**
    * 渲染匹配的结果
    * @param src 源文本
@@ -98,7 +105,7 @@ export type IExtension<> = {
    * 优先级
    * 当匹配的起始位置相同时，由优先级高的渲染
    */
-  priority?: number,
+  priority?: number;
 };
 /**
  * Markdown 标签
@@ -108,32 +115,32 @@ export type ITag = {
    * 标签名
    * 存在名称相同的标签时，旧标签将被覆盖
    */
-  name: string,
+  name: string;
   /**
    * 级别
    * block：块状
    * inline：行内
    */
-  level: 'block' | 'inline',
+  level: 'block' | 'inline';
   /**
    * 标签闭合
    * 即使用该标签需要结束标签
    * 行内默认为 false
    * 块状默认为 true
    */
-  end: IEmpty | boolean,
+  end: IEmpty | boolean;
   /**
    * 渲染匹配的结果
    * @param src 源文本
    * @param variables 渲染时可能需要使用的变量
    * @returns 返回 HTML 格式的字符串
    */
-  render(matched: IMatched, variables: IVariables): IAsyncReturnValue<string>,
+  render(matched: IMatched, variables: IVariables): IAsyncReturnValue<string>;
   /**
    * 优先级
    * 当匹配的起始位置相同时，由优先级高的渲染
    */
-  priority?: number,
+  priority?: number;
 };
 /**
  * 扁平目录项
@@ -142,15 +149,15 @@ export type IFlatToc = {
   /**
    * 名称
    */
-  name: string,
+  name: string;
   /**
    * 锚
    */
-  id: string,
+  id: string;
   /**
    * 级别
    */
-  level: number,
+  level: number;
 };
 /**
  * 树状目录项
@@ -159,22 +166,22 @@ export type ITreeToc = {
   /**
    * 名称
    */
-  name: string,
+  name: string;
   /**
    * 锚
    */
-  id: string,
+  id: string;
   /**
    * 子项
    */
-  child: ITreeToc[],
+  child: ITreeToc[];
 };
 /**
  * 普通目录对象
  */
 export type INormalToc = {
-  flat: IFlatToc[],
-  tree: ITreeToc[],
+  flat: IFlatToc[];
+  tree: ITreeToc[];
 };
 /**
  * 注册扩展
@@ -192,14 +199,20 @@ export function IRegisterTags(tags: ITag[]): void;
  * @param src Markdown 源文本
  * @param variables 渲染时可能需要使用的变量
  */
-export function IRenderLine(src: string, variables?: IVariables): Promise<{content: string, variables: IVariables}>;
+export function IRenderLine(
+  src: string,
+  variables?: IVariables
+): Promise<{ content: string; variables: IVariables }>;
 /**
  * 渲染 Markdown 文本
  * 主要用于渲染整段和块状的 Markdown 文本，所以拓展都会生效
  * @param src Markdown 源文本
  * @param variables 渲染时可能需要使用的变量
  */
-export function IRender(src: string, variables?: IVariables): Promise<{content: string, variables: IVariables, toc: INormalToc}>;
+export function IRender(
+  src: string,
+  variables?: IVariables
+): Promise<{ content: string; variables: IVariables; toc: INormalToc }>;
 /**
  * 目录类
  * 用于创建目录并记录 id，防止 id 冲突
