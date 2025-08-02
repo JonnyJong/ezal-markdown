b2638dd05e919623519a7545c0006f0bca86e678
Jonny <jonnyjong@outlook.com>
2025-08-02 16:50:54 +0800

    🐞 fix: Export define of plugin html

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,7 @@ export type { ImageParsed, ImageRefParsed } from './plugins/image';
 export type { ListParsed, TaskListParsed } from './plugins/list';
 export type { TableParsed } from './plugins/table';
 export type { TexParsed, TexOptions } from './plugins/tex';
+export type { HTMLRenderOptions, HTMLParsed } from './plugins/html';
 export type { Slugifier } from './anchor';
 export type {
 	RenderContext,
